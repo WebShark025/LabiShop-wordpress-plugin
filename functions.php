a282c0648dfdb3f4bc57f32e48a8afe81ad08fb0
WebShark025 <arshshia@gmail.com>
2015-10-20 19:23:56 +0330

    Created index file

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: Labishop
+   Plugin URI: http://sadeco.ir
+   Description: Simple Unique plugin for LabiShop. Describing Their Orders
+   Version: 1.0.0
+   Author: WebShark25
+   Author URI: http://sadeco.ir
+   License: GPL2
+   */
+?>
