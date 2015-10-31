686f5fbba8abdaef927e4a8d06d1bb3d294418d5
WebShark025 <arshshia@gmail.com>
2015-10-31 18:41:00 +0330

    Created and finished Database Creation File!

---
+++ db-table-creation.php
@@ -0,0 +1,35 @@
+<?php
+CREATE DB :
+
+$servername = "localhost";
+$username = "labishop_dbb";
+$password = "Passwd";
+$dbname = "labishop_dbb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE Orders (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+itemname VARCHAR(150) NOT NULL,
+fullname VARCHAR(150) NOT NULL,
+isdone VARCHAR(150) NOT NULL,
+get_id VARCHAR(150) NOT NULL,
+address VARCHAR(300) NOT NULL,
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table Orders created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+
+?>
