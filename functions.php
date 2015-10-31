433fcab5d0f35f60c70bd928d9780bda7fe7f77d
WebShark025 <arshshia@gmail.com>
2015-10-31 18:57:32 +0330

    Finished functions file (added form)

--- functions.php
+++ functions.php
@@ -134,7 +134,7 @@ add_action( 'save_post', 'labishop_save_meta_box_data' );
    function ls_addcont($content) {
    	$get_itemname = get_post_meta( get_the_ID(), 'ls_itemname', true );
    	$get_price = get_post_meta( get_the_ID(), 'ls_price', true );
-	$content .= 'Stuff to add after content';
+	$content .= include ('form.html');
 	return $content;
    }
 
