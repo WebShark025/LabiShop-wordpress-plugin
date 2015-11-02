6ee32e89cc4e1f7bec1cdd330d3c4e85efa847d6
WebShark025 <arshshia@gmail.com>
2015-11-02 15:46:36 +0330

    CONFIG FILE !

---
+++ config-new.php
@@ -0,0 +1,27 @@
+<?php
+
+// This is the CONFIG FILE. Enter the values please ! COPY THIS AT THE PLUGIN ROOT FOLDER & WHERE YOU COPY go.php & get.php FILES!!!!!! BE SURE TO DO THAT
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// MASTER WARN!!! /\
+
+$API_KEY = "82398-583eb-d2655-6344d-3b1eabb8a0f94705a4e91eb2a184";
+$ROOT_URL = "http://cafe.labishop.com";
+$DATABASE_HOST
+$DATABASE_USERNAME
+$DATABASE_DBNAME
+$DATABASE_USER_PASSWORD
+
+// DO NOT !!! NOT !!!!! NOT !!! EDIT
+
+$GO_URL = $ROOT_URL."/go.php";
+$GET_URL = $ROOT_URL."/get.php";
+
+?>
